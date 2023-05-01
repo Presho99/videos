a2ea3aba9957708f0ec2e8057c8b9ef790387760
Precious <Preshomondi@gmail.com>
2023-05-01 18:56:13 +0300

    Add semantic css

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Searchbar from './Searchbar'
 
 class App extends React.Component{
     render(){
         return (
-            <div>App</div>
+            <div>
+                <Searchbar/>
+            </div>
           )
     }
  
---
+++ src/components/Searchbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Searchbar extends React.Component {
+    render(){
+        return (
+            <div>Searchbar</div>
+          )
+    }
+  
+}
+
+export default Searchbar
