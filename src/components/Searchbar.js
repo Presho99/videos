b8472232d18bbdebee6f6a3a1d2462ad0ad5f89b
Precious <Preshomondi@gmail.com>
2023-05-02 19:35:17 +0300

    Fetch videos

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React from "react";
 
 class Searchbar extends React.Component {
-    render(){
-        return (
-            <div>Searchbar</div>
-          )
+    state = {term: ""}
+
+    onInputChange =(e) => {
+        this.setState({term : e.target.value})
     }
-  
+
+    onFormSubmit = (e) => {
+       e.preventDefault() 
+       this.props.onSubmitting(this.state.term)
+
+    // Call callback from parent component
+    }
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+              <label>Video Search</label>
+              <input 
+              type="text" 
+              value={this.state.term}
+              onChange={this.onInputChange}
+              />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Searchbar
+export default Searchbar;
